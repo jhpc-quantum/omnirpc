e0bd9008600a7b560d7cd6aa5e8f8a091314ec57
m-hirano <m-hirano@sra.co.jp>
2023-03-28 19:11:45 +0900

    Works.

--- qcrpc/qcmeasure/qcmeasure.c
+++ qcrpc/qcmeasure/qcmeasure.c
@@ -1,53 +1,21 @@
-// Test without OmniRPC
-
 #include "omni_platform.h"
 #include "qcs_api.hpp"
 
-int main(int argc, char *argv[])
-{
-  int qubits = 5;
-  bool do_save = false;
-  bool do_load = false;
-  bool do_rpc = false;
-  bool do_job_submit = false;
-  
-  if (argc == 2 && strcmp(argv[1], "-s") == 0) {
-    do_save = true;
-  } else if (argc == 2 && strcmp(argv[1], "-l") == 0) {
-    do_load = true;
-  } else if (argc == 2 && strcmp(argv[1], "-re") == 0) {
-    do_rpc = true;
-  } else if (argc == 2 && strcmp(argv[1], "-rs") == 0) {
-    do_job_submit = true;
-    do_rpc = true;
-  }
+int
+main(int argc, char *argv[]) {
+  if (likely(argc > 2)) {
+    int nqubits = atoi(argv[1]);
+    char *file = argv[2];
 
-  if (do_rpc == true) {
-    QC_InitRemote(&argc, &argv);
-  }
-  
-  QC_Init(&argc, &argv, qubits, 0); // 0 = qulacs
-
-  if (do_load == false) {
-
-    HGate(0);
-    U1Gate(0.1, 0);
-    U2Gate(0.1, 0.2, 1);
-    U3Gate(0.1, 0.2, 0.3, 2);
-
-    if (do_save == true) {
-      QC_SaveContext("test.dump");
-    } else if (do_rpc == true) {
-      QC_MeasureRemote(do_job_submit);
+    if (likely(nqubits > 0 && file != NULL && *file != '\0')) {
+      QC_Init(&argc, &argv, nqubits, 0);
+      QC_LoadContext(file);
+      QC_Measure();
+      QC_Finalize();
+      return 0;
     }
-  } else {
-    QC_LoadContext("test.dump");
   }
 
-  if (do_save == false && do_rpc == false) {
-    QC_Measure();
-  }
-
-  QC_Finalize();
+  return 1;
 }
 
