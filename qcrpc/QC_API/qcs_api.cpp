b353e70129e8eb05b69e08e68ce1e8519ae9dc47
m-hirano <m-hirano@sra.co.jp>
2024-01-22 10:45:17 +0900

    Improved debug output.

--- qcrpc/QC_API/qcs_api.cpp
+++ qcrpc/QC_API/qcs_api.cpp
@@ -162,15 +162,16 @@ QC_MeasureRemoteQASMStringREST(const char *url,
         int rlen = 0;
         char remark[65536];
 
-        if ((url != NULL && (ulen = strlen(url)) > 0) &&
-            (token != NULL && (tlen = strlen(token)) > 0) &&
-            (qasm != NULL && (qlen = strlen(qasm)) > 0) &&
+        if ((url != NULL && (ulen = (strlen(url) + 1)) > 1) &&
+            (token != NULL && (tlen = (strlen(token) + 1)) > 1) &&
+            (qasm != NULL && (qlen = (strlen(qasm) + 1)) > 1) &&
             (qc_type >= 0) &&
             (shots > 0 && poll_ms > 0 && poll_max >= 0) &&
             (transpiler >= 0) &&
             (out != NULL && maxout > 1) &&
-            olen != NULL) {
+            (olen != NULL)) {
             int ret = -INT_MAX;
+            int getlen = -INT_MAX;
 
             if (rem != NULL && (rlen = strlen(rem)) > 0) {
                 snprintf(remark, sizeof(remark), "%s", rem);
@@ -182,11 +183,12 @@ QC_MeasureRemoteQASMStringREST(const char *url,
 
             fprintf(stderr, "debug: rem %d, '%s'\n", rlen, rem);
 
+            *olen = -INT_MAX;
             OmniRpcRequest r =
                 OmniRpcCallAsync("qc_rpc_rest_qasm_strig",
-                                 /* 00, 01 */ ulen + 1, url,
-                                 /* 02, 03 */ tlen + 1, token,
-                                 /* 04, 05 */ qlen + 1, qasm,
+                                 /* 00, 01 */ ulen, url,
+                                 /* 02, 03 */ tlen, token,
+                                 /* 04, 05 */ qlen, qasm,
                                  /* 06     */ qc_type,
                                  /* 07, 08 */ rlen, remark,
                                  /* 09     */ shots,
@@ -196,8 +198,13 @@ QC_MeasureRemoteQASMStringREST(const char *url,
 
                                  /* 13     */ &ret,
                                  /* 14, 15 */ maxout, out,
-                                 /* 16     */ olen);
+                                 /* 16     */ &getlen);
             OmniRpcWait(r);
+
+            fprintf(stderr, "debug: ret %d, getlen %d\n", ret, getlen);
+            if (ret >= 0) {
+                *olen = getlen;
+            }
         }
     }
 }
