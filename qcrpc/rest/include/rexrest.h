b76563b0906d2956e9f7dd4451be060dd6960028
Takaya Hasegawa <takaya@sra.co.jp>
2024-03-14 20:33:34 +0900

    Add process to scrape the results

--- qcrpc/rest/include/rexrest.h
+++ qcrpc/rest/include/rexrest.h
@@ -7,10 +7,13 @@
 #ifdef __cplusplus
 extern "C" {
 #endif // __cplusplus
-int calculate(uint32_t rex_type, const char *base_url, const char *token, const char *qasm,
+int calculate(uint32_t qc_type, const char *base_url, const char *token, const char *qasm,
               uint32_t shots, uint32_t transpiler, const char *remark,
               uint32_t polling_interval, uint32_t max_polling_count,
               char **output, size_t *output_len);
+
+int scrape_response(uint32_t qc_type, const char *resp, uint32_t shots, int **patterns,
+                    float **probs, size_t *n_patterns);
 
 #ifdef __cplusplus
 }
